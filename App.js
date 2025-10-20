c0b1593f126533ecf37f93a97880067e104281e9
Jose Luis <wildchamo@gmail.com>
2025-10-20 12:34:40 -0500

    refactor: simplify App component by removing text, styles and button elements

--- App.js
+++ App.js
@@ -3,37 +3,11 @@ import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola, Jose!</Text>
-      <Text style={styles.container.dummyText}>Hola, Jose!</Text>
-      <Text style={styles.dummyText}>Hola, Jose 2!</Text>
-      <Button title="Click me" styles={styles.button} />
+    <View>
+      <View></View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "cyan",
-    alignItems: "center",
-    justifyContent: "center",
-
-    dummyText: {
-      margin: 150,
-    },
-  },
-  button: {
-    backgroundColor: "red",
-    color: "white",
-  },
-  dummyText: {
-    color: "red",
-    fontSize: 20,
-    fontWeight: "bold",
-    margin: 15,
-    borderWidth: 2,
-    borderColor: "black",
-    padding: 14,
-  },
-});
+const styles = StyleSheet.create({});
