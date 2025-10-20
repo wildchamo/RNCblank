7bf2c67340b6b3279f3e45ad46332412dc1fdcc4
Jose Luis <wildchamo@gmail.com>
2025-10-20 12:39:07 -0500

    feat: add basic goal input form and list layout with styling

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <View></View>
-      <View></View>
+    <View style={styles.appContainer}>
+      <View>
+        <Text>Text</Text>
+        <TextInput placeholder="Your course goal!"></TextInput>
+        <Button title="Add Goal"></Button>
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 40,
+  },
+});
